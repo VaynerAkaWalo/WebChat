9e86513dc4df0af829b3e24c9b70d261df5c5727
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-05-22 13:14:48 +0200

    Message class

---
+++ Models/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace WebChat.Models;
+
+public class Message
+{
+    public string Username { get; }
+    public string Text { get; }
+    
+    public DateTime Date { get; }
+
+    public Message(string username, string text)
+    {
+        Date = DateTime.Now;
+        Username = username;
+        Text = text;
+    }
+}
