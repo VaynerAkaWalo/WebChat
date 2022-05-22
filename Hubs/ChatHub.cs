3ea519b9c6650c23c7507a17c863add1671b8db2
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-05-22 18:11:56 +0200

    added storage service and function to sens message history to client after connecting.

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.SignalR;
 using WebChat.Models;
 
 namespace WebChat.Hubs;
 
 public class ChatHub : Hub
 {
+    private readonly IMessageStorage _database;
+
+    public ChatHub(IMessageStorage database)
+    {
+        _database = database;
+    }
+    
     public async Task SendMessage(string user, string message)
     {
         var newMessage = new Message(user, message);
-        await Clients.All.SendAsync("ReceiveMessage", newMessage.Username, newMessage.Text, newMessage.Date);
+        _database.NewMessage(newMessage);
+        string json = JsonSerializer.Serialize(newMessage);
+        await Clients.All.SendAsync("ReceiveMessage", json);
+    }
+
+    public async Task Connected()
+    {
+        string json = JsonSerializer.Serialize(_database.Messages());
+        await Clients.Client(Context.ConnectionId).SendAsync("LoadMessages", json);
     }
 }
