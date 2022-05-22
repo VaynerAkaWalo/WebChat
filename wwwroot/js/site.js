9314757ef5870a2a19493f616f7a868f4ee2fa3f
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-05-22 02:47:08 +0200

    dialog box scripts

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+"use strict";
+var connected = false;
+var nickname = "";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("send-message-button").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    li.classList.add("message-li");
+    document.getElementById("message-list").appendChild(li);
+    var time = new Date();
+    li.textContent = `${time.getHours()}:${time.getMinutes()} ${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    connected = true;
+}).catch(function (err) {
+    return console.error(err.toString());
+})
+
+document.getElementById("send-message-button").addEventListener("click", function (event) {
+    var message = document.getElementById("message-input-box").value;
+    document.getElementById("message-input-box").value = "";
+    connection.invoke("SendMessage", nickname, message).catch(function (err) {
+        return console.error(err.toString());
+    })
+})
+
+document.getElementById("nickname-button").addEventListener("click", function (event) {
+    if(connected)
+    {
+        nickname = document.getElementById("nickname-input").value;
+        if(typeof nickname === "string")
+        {
+            if(nickname !== "")
+            {
+                document.getElementById("send-message-button").disabled = false;
+                document.getElementById("nickname-dialog").style.visibility = "hidden";
+            }
+        }
+    }
+})
