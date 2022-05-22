d3a6d53753e3f3cf87a10240c45a7191ef741cc7
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-05-22 19:12:05 +0200

    New IMessageStorage implementation based on queue

---
+++ Models/QueueBasedStorage.cs
@@ -0,0 +1,28 @@
+﻿namespace WebChat.Models;
+
+public class QueueBasedStorage : IMessageStorage
+{
+    private readonly static int Size = 30;
+
+    private readonly Queue<Message> _database;
+
+    public QueueBasedStorage()
+    {
+        _database = new Queue<Message>();
+    }
+    
+    public void NewMessage(Message message)
+    {
+        if (_database.Count == 30)
+        {
+            _database.Dequeue();
+        }
+
+        _database.Enqueue(message);
+    }
+
+    public IEnumerable<Message> Messages()
+    {
+        return _database;
+    }
+}
