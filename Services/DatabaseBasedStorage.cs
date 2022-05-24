7a7c13e04bace9c1e9c97d75d1e957ead4725af6
VaynerAkaWalo <vaynerakawalo@gmail.com>
2022-05-25 01:06:52 +0200

    database storage implementation

---
+++ Services/DatabaseBasedStorage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebChat.Models;
+
+namespace WebChat.Services;
+
+
+public class DatabaseBasedStorage : IMessageStorage
+{
+    private readonly MessageContext _context;
+
+    public DatabaseBasedStorage(MessageContext messageContext)
+    {
+        _context = messageContext;
+    }
+    
+    public void NewMessage(Message message)
+    {
+        _context.Messages.Add(message);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<Message> Messages()
+    {
+        return _context.Messages.ToList();
+    }
+}
---
+++ Services/MessageContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebChat.Models;
+namespace WebChat.Services;
+
+public class MessageContext : DbContext
+{
+    public MessageContext(DbContextOptions<MessageContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Message> Messages { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("webchat");
+        modelBuilder.Entity<Message>().ToTable("messages");
+    }
+}
